b4f99cb92c5c3576caae4acbed58a4da044e54ee
fdankwah <fredrick@appsnmobilesolutions.com>
2024-06-24 09:46:39 +0000

    added about

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavMenuComponent } from '../../components/nav-menu/nav-menu.component';
 import { HomeComComponent } from '../../components/home-com/home-com.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { HomeComComponent } from '../../components/home-com/home-com.component';
   imports: [
     NavMenuComponent,
     HomeComComponent,
+    AboutComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
